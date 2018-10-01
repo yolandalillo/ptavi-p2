8007b7375f3812b9df322bab4f56a5e27127edab
Yolanda Lillo Mata <ylillo@gamma09.aulas.gsyc.urjc.es>
2018-10-01 16:01:42 +0200

    con el fichero cerrado

--- calcplus.py
+++ calcplus.py
@@ -6,58 +6,39 @@ Created on Wed Sep 26 13:12:35 2018
 @author: ylillo
 """
 import sys
-
+import calcoohija
 
 fichero = './fichero'
 
 
-class Calculadora:
-    def plus(listanumeros):
-        suma = listanumeros[0]
-        for i in listanumeros[1:]:
-            suma += i
-        return suma
-
-    def minus(listanumeros):
-        resta = listanumeros[0]
-        for i in listanumeros[1:]:
-            resta -= i
-        return resta
-
-
-class CalculadoraHija(Calculadora):
-    def multiplication(listanumeros):
-        multiplication = 1
-        for i in listanumeros[1:]:
-            multiplication *= i
-        return multiplication
-
-    def division(listanumeros):
-        division = 1
-        try:
-            for i in listanumeros[1:]:
-                division /= i
-            return division
-        except ZeroDivisionError as err:
-            return 'Division by zero is not allowed'
-
-
 if __name__ == "__main__":
-    with open(fichero, 'r') as reader:
-        for line in reader:
-            lista = line.split(',')
-            lista = lista[:-1]
-            listanumeros = list(map(int, lista[1:]))
+    fd = open("fichero", encoding='utf-8', mode="r")
+    micalchija = calcoohija.CalculadoraHija()
+    for line in fd:
+        lista = line.split(',')
+        listanumeros = list(map(int, lista[1:]))
 
-            if lista[0] == 'suma':
-                Total = CalculadoraHija.plus(listanumeros)
-            elif lista[0] == "resta":
-                Total = CalculadoraHija.minus(listanumeros)
-            elif lista[0] == 'multiplica':
-                Total = CalculadoraHija.multiplication(listanumeros)
-            elif lista[0] == 'divide':
-                Total = CalculadoraHija.division(listanumeros)
-            else:
-                sys.exit('Operación sólo puede ser sumar o restar.')
+        if lista[0] == 'suma':
+            op1 = 0
+            for op2 in listanumeros:
+                op1 = micalchija.plus(op1, op2)
+        elif lista[0] == "resta":
+            op1 = listanumeros[0]
+            for op2 in listanumeros[1:]:
+                op1 = micalchija.minus(op1, op2)
+        elif lista[0] == 'multiplica':
+            op1 = 1
+            for op2 in listanumeros:
+                op1 = micalchija.multiplication(op1, op2)
+        elif lista[0] == 'divide':
+            try:
+                op1 = listanumeros[0]
+                for op2 in listanumeros[1:]:
+                    op1 = micalchija.division(op1, op2)
+            except ZeroDivisionError as err:
+                print('Division by zero is not allowed')
+        else:
+            sys.exit('Operación sólo puede ser sumar o restar.')
 
-            print(lista[0], Total)
+        print(lista[0], op1)
+    fd.close()
