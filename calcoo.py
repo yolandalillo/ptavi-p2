7dcac132b25711a21a8e11874477eec3247c7392
Yolanda Lillo Mata <ylillo@gamma19.aulas.gsyc.urjc.es>
2018-10-01 10:39:25 +0200

    Apartado 2 con pep8

--- calcoo.py
+++ calcoo.py
@@ -8,19 +8,22 @@ Este es un archivo temporal
 
 import sys
 
+
 class Calculadora:
-    def plus(self,op1,op2):
+    def plus(self, op1, op2):
         return op1 + op2
-    def minus(self,op1,op2):
+
+    def minus(self, op1, op2):
         return op1 - op2
-    
+
+
 if __name__ == "__main__":
     micalc = Calculadora()
     try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
-        Calculadora1 = micalc.plus(operando1,operando2)
-        Calculadora2 = micalc.minus(operando1,operando2)
+        Calculadora1 = micalc.plus(operando1, operando2)
+        Calculadora2 = micalc.minus(operando1, operando2)
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
@@ -30,7 +33,5 @@ if __name__ == "__main__":
         Calculadora = micalc.minus(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
-    
-    print(Calculadora)
 
-    
+    print(Calculadora)
