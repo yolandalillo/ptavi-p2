f577eceb701387782e09e53dac455c51d5442b12
Yolanda Lillo Mata <ylillo@gamma09.aulas.gsyc.urjc.es>
2018-10-01 16:59:47 +0200

    Final correguido division entre cero

--- calcplus.py
+++ calcplus.py
@@ -21,25 +21,24 @@ if __name__ == "__main__":
         lista = line.split(',')
         listanumeros = list(map(int, lista[1:]))
 
+        op1 = listanumeros[0]
         if lista[0] == 'suma':
-            op1 = 0
-            for op2 in listanumeros:
+            for op2 in listanumeros[1:]:
                 op1 = micalchija.plus(op1, op2)
         elif lista[0] == "resta":
             op1 = listanumeros[0]
             for op2 in listanumeros[1:]:
                 op1 = micalchija.minus(op1, op2)
         elif lista[0] == 'multiplica':
-            op1 = 1
-            for op2 in listanumeros:
+            for op2 in listanumeros[1:]:
                 op1 = micalchija.multiplication(op1, op2)
         elif lista[0] == 'divide':
-            try:
-                op1 = listanumeros[0]
-                for op2 in listanumeros[1:]:
+            for op2 in listanumeros[1:]:
+                if op1 == 0 or op2 == 0:
+                    print('Division by zero is not allowed')
+                else:
                     op1 = micalchija.division(op1, op2)
-            except ZeroDivisionError as err:
-                print('Division by zero is not allowed')
+
         else:
             sys.exit('Operación sólo puede ser sumar o restar.')
 
